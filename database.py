66e36a278b68380b868de5e780a2a8c1f94a545d
Arnaldo Oliveira Meirelles <meirelles21arnaldo@gmail.com>
2026-08-20 05:05:46 -0300

    Implement student database management functions
    This script provides functions to connect to a SQLite database, create a table for students, and perform CRUD operations (create, read, update, delete) on student records.

---
+++ database.py
@@ -0,0 +1,93 @@
+import sqlite3 as sql
+#importa o sql
+
+#cria uma funcao para se conectar ao banco de dados
+def conectar():
+    conn = sql.connect("escola.db")
+    return conn
+
+
+#cria funcao para criar tabela
+def criar_tabela():
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS alunos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        idade INTEGER NOT NULL,
+        nota REAL)
+    """)
+
+    conn.commit()
+    conn.close()
+
+#cadastra um novo aluno
+def cadastrar_aluno(nome , idade , nota):
+
+    conn = conectar()
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO alunos (nome, idade, nota) VALUES (? , ? , ?)" , (nome , idade , nota))
+
+    conn.commit()
+    conn.close()
+
+
+#busca um aluno por nome
+def buscar_aluno_nome(busca_nome):
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    #a vírgula é pq é uma tupla de 1
+    cursor.execute("SELECT * FROM alunos WHERE nome = ?" , (busca_nome,))
+
+    #pega a lista e guarda em alunos
+    alunos = cursor.fetchall()
+
+    #sem commit já que não altera o banco de dados
+    conn.close()
+
+    return alunos
+
+#busca um aluno por ID
+def buscar_aluno_id(busca_id):
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    #a vírgula é pq é uma tupla de 1
+    cursor.execute("SELECT * FROM alunos WHERE id = ?" , (busca_id,))
+
+    #pega a lista e guarda em alunos
+    alunos = cursor.fetchall()
+
+    #sem commit já que não altera o banco de dados
+    conn.close()
+
+    return alunos
+
+#mudar aluno
+def atualizar_aluno(nome , idade , nota , id):
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE alunos SET nome = ? , idade = ? , nota = ? WHERE id = ?" , (nome , idade , nota , id))
+
+    conn.commit()
+    conn.close()
+
+#deletar aluno
+def deletar_aluno(id):
+    
+    conn = conectar()
+    cursor = conn.cursor()
+    
+    cursor.execute("DELETE FROM alunos WHERE id = ?" , (id,))
+
+    conn.commit()
+    conn.close()
